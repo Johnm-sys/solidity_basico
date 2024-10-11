0fbdfd1133ac5935d1c4509880d1ba533ae14d1b
Johnm-sys <jhonmenjura12@gmail.com>
2024-10-11 10:47:21 -0500

    primer contrato

---
+++ hoola_mundo.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24; // en esta parte defino la version del copilador
+
+// my contrato   0xAa71F20262A89cB563F23eaDB63432176fA56fC8
+
+
+// apartir de aqui estamos diciendo que comienza la logica de mi contrato
+contract hola_mundo { 
+    
+    string public saludo = "hola desde educateth john version 2"; // mi primera variable fuera del contrato (publica)
+
+
+}
